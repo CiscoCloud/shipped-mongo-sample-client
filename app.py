0caed55dfe2e463584984eb29a13b9ba61948848
SomnathNiroula <javasomnath@gmail.com>
2016-08-02 12:41:06 -0400

    removed reloading on every click

--- app.py
+++ app.py
@@ -7,54 +7,49 @@ import json
 app = Flask(__name__)
 id = []
 
+# returns the UI
 @app.route("/")
 def hello():
-    id = setupDB()
+	#getting a mongodb connection
     db = dbConnection()
+	#get an existing table
     if "tbl_counter" in db.collection_names():
         tbl_counter = db.get_collection('tbl_counter')
-    cursor = tbl_counter.find()
-    result = {}
-    for entry in cursor:
-        result = entry
-        i = result['counter']
-        return render_template("index.html", count=i)
+		#get no of likes
+        result = (tbl_counter.find_one())
+    return render_template("index.html", count=result['counter'])
 
-@app.route("/count",  methods=['GET'])
-def getData():
-    db = dbConnection()
-    if "tbl_counter" in db.collection_names():
-        tbl_counter = db.get_collection('tbl_counter')
-        cursor = tbl_counter.find()
-        result={}
-        for entry in cursor:
-            result=entry
-            i = result['counter']
-            return render_template("index.html", count = i)
-
-
+#posts the count of likes
 @app.route("/like",  methods=['POST'])
 def postData():
+	#getting a mongodb connection
     db = dbConnection()
+	#get an existing table
     if "tbl_counter" in db.collection_names():
         tbl_counter = db.get_collection('tbl_counter')
-
+		#update vale of likes to current no. of like + 1
         tbl_counter.update(tbl_counter.find_one(), {'$inc': {'counter':1}},  upsert=False)
+		#get no of likes
         result = (tbl_counter.find_one())
-        return render_template("index.html", count = result['counter'])
+        return str(result['counter'])
 
+#resets like to 0
 @app.route("/reset",  methods=['POST'])
 def resetData():
+	#getting a mongodb connection
     db = dbConnection()
+	#get an existing table
     if "tbl_counter" in db.collection_names():
         tbl_counter = db.get_collection('tbl_counter')
-
+		#update vale of likes to 0
         tbl_counter.update(tbl_counter.find_one(), {'$set': {'counter': 0}}, upsert=False)
+		#get no of likes
         result = (tbl_counter.find_one())
-        return render_template("index.html", count = result['counter'])
+        return str(result['counter'])
 
 
 if __name__ == "__main__":
+    setupDB()
     app.run(debug=True,host='0.0.0.0')
 
 
--- utils.py
+++ utils.py
@@ -2,28 +2,30 @@ from pymongo import MongoClient, errors
 from bson import ObjectId
 import os
 
-
+#default URI for local bootstap from CLI
 MONGODB_URI = "mongodb://mongodb:27017@admin:mongo/mongo"
-
+#intial data that needs to be seeded
 SEED_DATA = [{'counter':0}]
 id = ""
 client = None
 
 def dbConnection():
+	#gets environment variable "DEPLOY_TARGET" value 
     deployTarget = os.environ.get('DEPLOY_TARGET')
+	#gets environment variable "HOST_RABBITMQ" value
     constr = os.environ.get('HOST_MONGODB_SINGLE')
+	#Retries to connect to Db for 10 times before throwing an error
     for i in range(0, 10):
         if i > 0:
             print("DB connection attempt %d of 10 failed; retrying (%s) connect string (%s)", i)
 
         # ex: "mongodb://mongodb:27017@admin:mongo/mongo?sslmode=disable"
+		#checks whether it is a local bootstrap or being deployed some centralized server
         if deployTarget == "LOCAL_SANDBOX":
             constr = MONGODB_URI
 
-
-        print("Current deploy target %s" % deployTarget)
-        #print(connstr)
         try:
+			#creates alient with embedded mongo db
             client = MongoClient(constr)
             db = client.get_default_database()#['example-db']
             return db
@@ -33,7 +35,7 @@ def dbConnection():
 
 def setupDB():
     db = dbConnection()
-
+	#checks if table exists or not before creating one
     if "tbl_counter" in db.collection_names():
         tbl_counter = db.get_collection('tbl_counter')
     else:
